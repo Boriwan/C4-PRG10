81e9b0b6f2eff36e9e4b3900614788f79b6a5da4
Borek2222 <boris.bocek@educanet.cz>
2022-01-19 19:25:23 +0100

    progress

--- Coor.java
+++ Coor.java
@@ -1,23 +1,38 @@
 import java.util.Objects;
 
 public class Coor {
-    private final int x;
-    private final int y;
-    int houses;
+    private int x;
+    private  int y;
 
-    public Coor(int x, int y, int houses) {
+    public Coor(int x, int y) {
         this.x = x;
         this.y = y;
-        this.houses = houses;
     }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
 
     @Override
     public boolean equals(Object o) {
-    return false;
+        return false;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(x, y, houses);
+        return Objects.hash(x, y);
     }
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,28 @@
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        HashMap<Coor, Integer> map;
+        Coor coor = new Coor(0, 0);
+        int x = 0, y = 0;
+        Scanner sc = new Scanner("input.txt");
 
+        String input = "";
+        while (sc.hasNextLine())
+            input = input + "\n" + sc.nextLine();
+
+        char[] arr = input.toCharArray();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == '<')
+            coor.setX(x--);
+            if (arr[i] == '>')
+                coor.setX(x++);
+            if (arr[i] == 'v')
+                coor.setY(y--);
+            else
+                coor.setY(y++);
+        }
     }
 }
