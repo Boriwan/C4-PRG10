f66db7229997fd5dea29afd8f466281bf48b7fd3
Borek2222 <boris.bocek@educanet.cz>
2022-01-20 19:13:41 +0100

    done

--- Main.java
+++ Main.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("input.txt");
-        Scanner sc = new Scanner(file);
-        HashSet<Coor> set = new HashSet<>();
+        final File file = new File("input.txt");
+        final Scanner sc = new Scanner(file);
+        final HashSet<Coor> set = new HashSet<>();
 
         int x = 0, y = 0;
 
-        String input = sc.nextLine();
-        String[] split = input.split("");
+        final String input = sc.nextLine();
+        final String[] split = input.split("");
 
         for (String s : split) {
             switch (s) {
@@ -21,7 +21,7 @@ public class Main {
                 case "v" -> y -= 1;
                 case "^" -> y += 1;
             }
-            Coor coor = new Coor(x, y);
+            final Coor coor = new Coor(x, y);
             set.add(coor);
         }
         System.out.println(set.size()+1);
